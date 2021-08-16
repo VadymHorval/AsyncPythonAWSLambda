9c0cea7558ac5a3382347014fbb8610b61a57803
Vadym <vadym.horval@gmail.com>
2021-08-16 22:03:36 +0300

    test5

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,15 @@
 import asyncio
 
 # The AWS Lambda handler
+import json
+
+
 def lambda_handler(event, context):
     asyncio.run(main())
-    return ('World')
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Hello World")
+    }
 
 async def main():
     # Here you can await any awaitable
