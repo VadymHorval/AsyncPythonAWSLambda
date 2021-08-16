684b22412d456294ab1084d675c800201db63b28
Vadym <vadym.horval@gmail.com>
2021-08-16 15:36:53 +0300

    1st commit

---
+++ AsyncLambda.py
@@ -0,0 +1,12 @@
+import asyncio
+
+# The AWS Lambda handler
+def handler(event, context):
+    asyncio.run(main())
+
+async def main():
+    # Here you can await any awaitable
+    await asyncio.sleep(1)
+    print('Hello')
+    await asyncio.sleep(1)
+    print('World..')
