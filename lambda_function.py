22d319ccbf9977cbf1ec80ee826a8d0b1e3e2d89
Vadym <vadym.horval@gmail.com>
2021-08-18 11:11:56 +0300

    test6

--- lambda_function.py
+++ lambda_function.py
@@ -1,19 +1,35 @@
 import asyncio
-
-# The AWS Lambda handler
 import json
+import requests
+import collections
 
+TABLE_ID = 'app3DEEwui4OTkeDI'
+API_KEY = 'keywRoiRn61Sk3nvo'
+TABLE_NAME = 'MainTable'
+print_list = []
 
 def lambda_handler(event, context):
+
     asyncio.run(main())
-    return {
+    response = {
         "statusCode": 200,
-        "body": json.dumps("Hello World")
+        "body": json.dumps(str.join(print_list))
     }
+    return response
 
 async def main():
-    # Here you can await any awaitable
-    await asyncio.sleep(1)
-    print('Hello')
-    await asyncio.sleep(1)
-    print('World...')
+    table_url = f'https://api.airtable.com/v0/{TABLE_ID}/{TABLE_NAME}?fields%5B%5D=ID&fields%5B%5D=title'
+    HEADERS = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
+    }
+    r = requests.get(url=table_url, headers=HEADERS)  # get data
+    data = r.json()
+    r_buffer = collections.deque(maxlen=len(data['records']))  # create a ring buffer
+
+    for i in data['records']:  # init buffer
+        r_buffer.append(i['fields'].get('title'))
+
+    for j in range(len(r_buffer) + 3):  # init data for printing - one loop + three steps
+        print_list.append('|' + r_buffer[0] + '->' + r_buffer[1] + '->' + r_buffer[2] + '|')
+        r_buffer.rotate(1)
