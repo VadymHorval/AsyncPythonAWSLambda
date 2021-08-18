61cc4a55d43e7dbbcaed986e3ff1e506991a9dfb
Vadym <vadym.horval@gmail.com>
2021-08-18 19:22:55 +0300

    test6

--- lambda_function.py
+++ lambda_function.py
@@ -10,6 +10,10 @@ print_list = []
 r_str = ''
 
 def lambda_handler(event, context):
+    if event['count']:
+        event['count'] +=1
+    else:
+        event.updata(count = 0)
 
     asyncio.run(main())
     r_str = (' '.join(str(e) for e in print_list)) +"!!!!!!!!!!!!!ID:" + context.aws_request_id + json.dumps(event)
