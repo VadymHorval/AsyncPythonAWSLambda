ab041ffaa13d7770b47aa959710df79800a81774
Vadym <vadym.horval@gmail.com>
2021-08-19 02:56:05 +0300

    test6

--- lambda_function.py
+++ lambda_function.py
@@ -1,38 +1,48 @@
+"""Get data from URL
+Initializes the rings buffer data.
+Generates data for output from the ring buffer.
+Outputs data using lambda AWS and AWS API Gateway
+"""
 import asyncio
 import json
-import urllib3
 import collections
+import urllib3
+
 
 TABLE_ID = 'app3DEEwui4OTkeDI'
 API_KEY = 'keywRoiRn61Sk3nvo'
 TABLE_NAME = 'MainTable'
-print_list = ''
-count = 0
+PRINT_LIST = ''
+COUNT = 0
+
 
 def lambda_handler(event, context):
+    """ Lambdas handler method."""
     asyncio.run(main())
     response = {
         "statusCode": 200,
-        "body": print_list
+        "body": PRINT_LIST
     }
     return response
 
 async def main():
-    global print_list
-    global count
-    table_url = f'https://api.airtable.com/v0/{TABLE_ID}/{TABLE_NAME}?fields%5B%5D=ID&fields%5B%5D=title'
-    HEADERS = {
+    """ Generates data for output from the ring buffer. Initializes the data buffer from the URL"""
+    global PRINT_LIST
+    global COUNT
+    table_url = f'https://api.airtable.com/v0/{TABLE_ID}/' \
+                f'{TABLE_NAME}?fields%5B%5D=ID&fields%5B%5D=title'
+    headers = {
         "Authorization": f"Bearer {API_KEY}",
         "Content-Type": "application/json"
     }
     http = urllib3.PoolManager()
-    r = http.request('GET',url=table_url, headers=HEADERS)  # get data
-    data = json.loads(r.data.decode("utf-8"))
+    req = http.request('GET',url=table_url, headers=headers)  # get data
+    data = json.loads(req.data.decode("utf-8"))
     r_buffer = collections.deque(maxlen=len(data['records']))  # create a ring buffer
 
     for i in data['records']:  # init buffer
         r_buffer.append(i['fields'].get('title'))
 
-    r_buffer.rotate(count) #scroll the buffer
-    count += 1
-    print_list += ('|' + r_buffer[0] + '->' + r_buffer[1] + '->' + r_buffer[2] + '|::')
+    r_buffer.rotate(COUNT) #scroll the buffer
+    COUNT += 1
+    PRINT_LIST += ('|' + r_buffer[0] + '->' + r_buffer[1] + '->' + r_buffer[2] + '|::')
