8f79da2224a117b2750f1f8d6f2fd8e5f07cf7f8
Vadym <vadym.horval@gmail.com>
2021-08-18 11:40:59 +0300

    test6

--- lambda_function.py
+++ lambda_function.py
@@ -9,6 +9,15 @@ TABLE_NAME = 'MainTable'
 print_list = []
 
 def lambda_handler(event, context):
+
+    asyncio.run(main())
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(str.join(print_list))
+    }
+    return response
+
+async def main():
     table_url = f'https://api.airtable.com/v0/{TABLE_ID}/{TABLE_NAME}?fields%5B%5D=ID&fields%5B%5D=title'
     HEADERS = {
         "Authorization": f"Bearer {API_KEY}",
@@ -16,7 +25,7 @@ def lambda_handler(event, context):
     }
     http = urllib3.PoolManager()
     r = http.request('GET',url=table_url, headers=HEADERS)  # get data
-    data = json.loads(r.data())
+    data = json.loads(r.data)
     r_buffer = collections.deque(maxlen=len(data['records']))  # create a ring buffer
 
     for i in data['records']:  # init buffer
@@ -25,9 +34,3 @@ def lambda_handler(event, context):
     for j in range(len(r_buffer) + 3):  # init data for printing - one loop + three steps
         print_list.append('|' + r_buffer[0] + '->' + r_buffer[1] + '->' + r_buffer[2] + '|')
         r_buffer.rotate(1)
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps(str.join(print_list))
-    }
-
