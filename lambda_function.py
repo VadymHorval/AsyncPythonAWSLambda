26c5e0f80dba6fef34eabe5470acafe26053fa8a
Vadym <vadym.horval@gmail.com>
2021-08-18 18:28:41 +0300

    test6

--- lambda_function.py
+++ lambda_function.py
@@ -12,7 +12,9 @@ r_str = ''
 def lambda_handler(event, context):
 
     asyncio.run(main())
-    r_str = (' '.join(str(e) for e in print_list)) +"!!!!!!!!!!!!!ID:" + context.aws_request_id
+    r_str = (' '.join(str(e) for e in print_list))\
+            +"!!!!!!!!!!!!!ID:" + context.aws_request_id \
+            +"------Event:"+ event.get('queryStringParameters')
     response = {
         "statusCode": 200,
         "body": r_str
