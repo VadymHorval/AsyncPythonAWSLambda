224a3410278b4e4631378789fcac074e7903124b
Vadym <vadym.horval@gmail.com>
2021-08-19 02:14:24 +0300

    test6

--- lambda_function.py
+++ lambda_function.py
@@ -13,10 +13,10 @@ def lambda_handler(event, context):
     str_count = ''
     global count
 
-    for n in event["rawQueryString"]:
-        if n.isdigit():
-            str_count += n
-    count = int(str_count)
+    # for n in event["rawQueryString"]:
+    #     if n.isdigit():
+    #         str_count += n
+    # count = int(str_count)
 
     asyncio.run(main())
     #r_str = (' '.join(str(e) for e in print_list)) +"!!!!!!!!!!!!!ID:" + event["rawQueryString"] +":::Event:"+ json.dumps(event)
@@ -39,13 +39,11 @@ async def main():
     http = urllib3.PoolManager()
     r = http.request('GET',url=table_url, headers=HEADERS)  # get data
     data = json.loads(r.data.decode("utf-8"))
-    print(data)
-    print(data['records'])
     r_buffer = collections.deque(maxlen=len(data['records']))  # create a ring buffer
 
     for i in data['records']:  # init buffer
         r_buffer.append(i['fields'].get('title'))
 
     r_buffer.rotate(count)
-    print(r_buffer)
-    print_list = ('|' + r_buffer[0] + '->' + r_buffer[1] + '->' + r_buffer[2] + '|::')
+    count += 1
+    print_list += ('|' + r_buffer[0] + '->' + r_buffer[1] + '->' + r_buffer[2] + '|::')
